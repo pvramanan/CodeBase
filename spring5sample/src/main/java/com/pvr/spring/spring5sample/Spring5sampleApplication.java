eb36e378e6481235cc9626bfc6558a4af5b9d13d
unknown <learnprasanna@gmail.com>
2019-10-20 02:07:39 +0530

    PVRL-Spring5Learning : basic spring 5 in running condition

---
+++ spring5sample/src/main/java/com/pvr/spring/spring5sample/Spring5sampleApplication.java
@@ -0,0 +1,13 @@
+package com.pvr.spring.spring5sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5sampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5sampleApplication.class, args);
+	}
+
+}
---
+++ spring5sample/src/test/java/com/pvr/spring/spring5sample/Spring5sampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvr.spring.spring5sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5sampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
